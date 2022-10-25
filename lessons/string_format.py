7412687a62d8af219ccf009291f89d337579b40c
Alexa Hendry <alexahendry88@gmail.com>
2022-10-25 15:45:09 +0000

    string formatting

---
+++ lessons/string_format.py
@@ -0,0 +1,20 @@
+num1 = 3.1425467389
+num2 = 10.2903948
+
+# PREVIOUS
+# print('num 1 is', num1, 'and num 2 is', num2)
+
+# FORMAT METHOD
+# print('num 1 is {0} and num 2 is {1}'.format(num1, num2))
+
+# # print 3 digits only (precision option)
+# print('num 1 is {0:.3} and num 2 is {1:.3}'.format(num1, num2))
+
+# # print 3 float points
+# print('num 1 is {0:.3f} and num 2 is {1:.3f}'.format(num1, num2))
+
+# # USING F-STRINGS
+# print(f'num 1 is {num1} and num 2 is {num2}')
+
+# #  formatting options are the same as the option above
+# print(f'num 1 is {num1:.4f} and num 2 is {num2:.4f}')
