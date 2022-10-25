a1cd2b701f3146d6b83cb8f43e4581e848f856e3
Alexa Hendry <alexahendry88@gmail.com>
2022-10-25 16:11:46 +0000

    for loops

---
+++ lessons/loops.py
@@ -0,0 +1,15 @@
+ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
+
+# for ninja in ninjas:
+#     print(ninja)
+
+# slice method
+# for ninja in ninjas[1:3]:
+#     print(ninja)
+
+#  add a statement when the cycle arrives at yoshi
+# for ninja in ninjas:
+#     if ninja == 'yoshi':
+#         print(f'{ninja} - black belt')
+#     else:
+#         print(ninja)
