86843dcc14cb2f5cff3701f38c7488d2138a5a2f
Alexa Hendry <alexahendry88@gmail.com>
2022-10-26 15:06:30 +0000

    sorting and sets

--- lessons/dictionaries.py
+++ lessons/dictionaries.py
@@ -1,6 +1,17 @@
-def ninja_intro(dictionary):
-    for key, val in dictionary.items():
-        print(f'I am {key} and I am a {val} belt')
+def belt_count(dictionary):
+    # get the values from the belts dictionary ie the colours
+    belts = list(dictionary.values())
+    # get each individual item in the list
+    # for belt in belts:
+    # avoid repetition of duplicates on output by using set on belts to only allow one of each colour
+    for belt in set(belts):
+        # create variable num and count how many of each item are in the list
+        num = belts.count(belt)
+        print(f'There are {num} {belt} belts')
+
+# def ninja_intro(dictionary):
+#     for key, val in dictionary.items():
+#         print(f'I am {key} and I am a {val} belt')
 
 
 ninja_belts = {}
@@ -18,4 +29,5 @@ while True:
         break
 
 
-ninja_intro(ninja_belts)
+# ninja_intro(ninja_belts)
+belt_count(ninja_belts)
