f23d990bf9f664c6b716cced7d067aee217a4655
Alexa Hendry <alexahendry88@gmail.com>
2022-10-26 15:26:21 +0000

    the init function

--- lessons/classes.py
+++ lessons/classes.py
@@ -1,19 +1,45 @@
+# using specific values, on one planet
+# class Planet:
+
+#     def __init__(self):
+#         self.name = 'Hoth'
+#         self.radius = 200000
+#         self.gravity = 5.5
+#         self.system = 'Hoth System'
+
+#     def orbit(self):
+#         return f'{self.name} is orbiting in the {self.system}'
+
+
+# hoth = Planet()
+# print(f'Name is: {hoth.name}')
+# print(f'Radius is: {hoth.radius}')
+# print(f'The gravity is: {hoth.gravity}')
+# print(hoth.orbit())
+
+
+# allowing for customisation for each new instance of Planet() class
 class Planet:
 
-    def __init__(self):
-        self.name = 'Hoth'
-        self.radius = 200000
-        self.gravity = 5.5
-        self.system = 'Hoth System'
+    def __init__(self, name, radius, gravity, system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
 
     def orbit(self):
         return f'{self.name} is orbiting in the {self.system}'
 
 
-hoth = Planet()
+hoth = Planet('Hoth', 200000, 5.5, 'Hoth System')
 print(f'Name is: {hoth.name}')
 print(f'Radius is: {hoth.radius}')
 print(f'The gravity is: {hoth.gravity}')
 print(hoth.orbit())
 
-planetX = Planet()
+
+naboo = Planet('Naboo', 300000, 8, 'Naboo System')
+print(f'Name: {naboo.name}')
+print(f'Radius: {naboo.radius}')
+print(f'Gravity: {naboo.gravity}')
+print(naboo.orbit())
