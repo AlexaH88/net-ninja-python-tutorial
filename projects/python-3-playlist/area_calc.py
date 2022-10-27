f6d4afa2a0bd3372034cb0aa538470dfa53b5a13
Alexa Hendry <alexahendry88@gmail.com>
2022-10-27 13:13:41 +0000

    commit all changs

--- projects/python-3-playlist/area_calc.py
+++
@@ -1,8 +0,0 @@
-# name = input('Tell me your name, punk: ')
-# age = input('...and your age')
-# print(name, 'you are', age)
-
-# Calc the area of a circle
-radius = input('Enter the radius of your circle (m):')
-area = 3.142 * int(radius) ** 2
-print('The area of your cirlce is:', area)
